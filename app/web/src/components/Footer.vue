c9281dff1710bccfe33390c785f7edceffbd2559
eozkurt <37531079+eozkurt@users.noreply.github.com>
2018-12-20 00:17:58 +0300

    change1

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -17,7 +17,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Microsoft Cloud Workshop
+        Microsoft OSS Team 
       </div>
       <div class="col-lg-6">
       </div>
